7c14e5ed1f92405b3a63278d57c71ae539629cba
Andrey Zhylka <zhylka.ai@gmail.com>
2025-12-24 12:52:20 +0100

    Registration and FOD calculation pipeline

--- TumorGrowthToolkit/FK_FOD/reg_HCP_to_atlas_in_brats.sh
+++ TumorGrowthToolkit/FK_FOD/reg_HCP_to_atlas_in_brats.sh
@@ -9,6 +9,8 @@ python ${code_dir}/combine_5tt.py combine ${template_data_dir}/5tt_1.25.nii.gz $
 cd $regged_old_atlas_dir
 
 for subj_dir in BraTS*/
+# cd $out_folder
+# for subj_dir in */
 do
 subj_id="${subj_dir%/}"
 
@@ -32,10 +34,6 @@ rm ${subj_out}/data_rig.mif
 mrtransform ${template_data_dir}/5tt_combined.nii.gz ${subj_out}/5tt_combined_rig.nii.gz -linear ${subj_out}/hcp2atlas_in_brats_rig.txt -force
 
 /opt/fsl/bin/fslsplit ${subj_out}/data_rig.nii.gz ${subj_out}/data_rig_shell -t
-
-# antsRegistrationSyN.sh -t s -f ${regged_old_atlas_dir}/${subj_id}/transformed_t1.nii.gz -m ${subj_out}/data_rig_shell0000.nii.gz -o ${subj_out}/data_rig_SyN_shell0000 -n 15
-
-#antsRegistrationSyN.sh -t s -f ${regged_old_atlas_dir}/${subj_id}/transformed_t1.nii.gz -m ${subj_out}/T1w_masked_rig.nii.gz -o ${subj_out}/data_rig_SyN_shell0000 -n 15
 
 FIXED=${regged_old_atlas_dir}/${subj_id}/transformed_t1.nii.gz
 MOVING=${subj_out}/T1w_masked_rig.nii.gz
@@ -78,6 +76,8 @@ rm ${subj_out}/data_rig.nii.gz
 
 dwigradcheck ${subj_out}/trans_data_rig.nii.gz -fslgrad ${subj_out}/data_rig.bvec ${subj_out}/data_rig.bval -export_grad_mrtrix ${subj_out}/fixed_trans_data_rig.bvec -nthreads 20 -force
 
+dwi2tensor ${subj_out}/trans_data_rig.nii.gz ${subj_out}/trans_data_rig_dt.nii.gz -grad ${subj_out}/fixed_trans_data_rig.bvec
+
 antsApplyTransforms -i ${subj_out}/5tt_combined_rig.nii.gz -d 3 -e 0 \
    -t ${subj_out}/data_rig_SyN_shell00001Warp.nii.gz \
    -t ${subj_out}/data_rig_SyN_shell00000GenericAffine.mat \
@@ -92,9 +92,28 @@ dwi2response msmt_5tt ${subj_out}/trans_data_rig.nii.gz ${subj_out}/5tt_rig_SyN.
 
 dwi2fod msmt_csd ${subj_out}/trans_data_rig.nii.gz -grad ${subj_out}/fixed_trans_data_rig.bvec ${subj_out}/response_wm.txt ${subj_out}/wmfod.nii.gz ${subj_out}/response_gm.txt ${subj_out}/gm.nii.gz ${subj_out}/response_csf.txt ${subj_out}/csf.nii.gz -mask ${subj_out}/trans_data_rig_mask.nii.gz -nthreads 20 -force
 
+fod2fixel  ${subj_out}/wmfod.nii.gz ${subj_out}/fixels -nii -peak_amp amp.nii.gz -disp disp.nii.gz -nthreads 20 -force
+
+mrgrid ${subj_out}/trans_data_rig.nii.gz regrid -voxel 1.5 ${subj_out}/trans_data_rig_1.5.nii.gz
 rm ${subj_out}/trans_data_rig.nii.gz
 
-fod2fixel  ${subj_out}/wmfod.nii.gz ${subj_out}/fixels -nii -peak_amp amp.nii.gz -disp disp.nii.gz -nthreads 20 -force
+dwi2mask ${subj_out}/trans_data_rig_1.5.nii.gz -grad ${subj_out}/fixed_trans_data_rig.bvec ${subj_out}/trans_data_rig_mask_1.5.nii.gz -force
+dwi2tensor ${subj_out}/trans_data_rig_1.5.nii.gz ${subj_out}/trans_data_rig_1.5_dt.nii.gz \
+     -grad ${subj_out}/fixed_trans_data_rig.bvec -mask ${subj_out}/trans_data_rig_mask_1.5.nii.gz -force
+dwi2response msmt_5tt ${subj_out}/trans_data_rig_1.5.nii.gz ${subj_out}/5tt_rig_SyN.nii.gz \
+     ${subj_out}/response_wm_1.5.txt \
+     ${subj_out}/response_csf_1.5.txt \
+     ${subj_out}/response_gm_1.5.txt \
+     -grad ${subj_out}/fixed_trans_data_rig.bvec -nthreads 20 -info -force
+dwi2fod msmt_csd ${subj_out}/trans_data_rig_1.5.nii.gz -grad ${subj_out}/fixed_trans_data_rig.bvec \
+     ${subj_out}/response_wm_1.5.txt ${subj_out}/wmfod_1.5.nii.gz \
+     ${subj_out}/response_gm_1.5.txt ${subj_out}/gm_1.5.nii.gz \
+     ${subj_out}/response_csf_1.5.txt ${subj_out}/csf_1.5.nii.gz \
+     -mask ${subj_out}/trans_data_rig_mask_1.5.nii.gz -nthreads 20 -force
+
+rm ${subj_out}/trans_data_rig_1.5.nii.gz
+
+fod2fixel  ${subj_out}/wmfod_1.5.nii.gz ${subj_out}/fixels_15 -nii -peak_amp amp.nii.gz -disp disp.nii.gz -nthreads 20 -force
 
 done
 
